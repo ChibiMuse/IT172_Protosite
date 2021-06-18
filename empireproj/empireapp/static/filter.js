07eae2e0bf79015c326abc91f3f35386c5201ad2
ChibiMuse <megan.smith@live.com>
2021-06-17 23:52:03 -0700

    shop page updates

---
+++ empireproj/empireapp/static/filter.js
@@ -0,0 +1,10 @@
+var classVar;
+
+function filterToggle(classVar){
+    var filterEl = document.getElementsByClassName(classVar);
+    if (filterEl.style.display ==="none"){
+        Element.style.display = "block";
+    } else {
+        Element.style.display = "none";
+    }
+}
